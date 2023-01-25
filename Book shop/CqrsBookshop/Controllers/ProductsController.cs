1568d6b0afb28f90a311ebda771d9adbe73377c0
David Price <david.price10@hotmail.com>
2023-01-25 01:20:47 +0000

    Basic Implementation
    WIP
    
    WIP

---
+++ Book shop/CqrsBookshop/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CqrsBookshop.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
